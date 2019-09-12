9efdb5fc474402044b2b6c8976ec68b9af71b33e
liuyang_ly <liuyang_ly@youzan.com>
2019-09-12 14:50:01 +0800

    可以发布了，但是源码发布还没搞清状况

--- enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/EnjoyMavenPublishPlugin.groovy
+++ enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/EnjoyMavenPublishPlugin.groovy
@@ -7,6 +7,8 @@ import org.gradle.api.artifacts.maven.MavenDeployment
 import org.gradle.api.plugins.MavenPlugin
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.internal.impldep.org.apache.maven.Maven
 import org.gradle.plugins.signing.SigningPlugin
 
 /**
@@ -23,68 +25,65 @@ class EnjoyMavenPublishPlugin implements Plugin<Project> {
         }
 
         // add the needed plugin
-        targetProject.plugins.apply(MavenPlugin)
-        targetProject.plugins.apply(SigningPlugin)
         targetProject.plugins.apply(MavenPublishPlugin)
 
         MavenPublishExt publishExt = targetProject.extensions.create("mavenPublish", MavenPublishExt)
+
         targetProject.afterEvaluate {
-            // 添加上传构件的task，并定义task的依赖关系
-            targetProject.uploadArchives {
-                println("publish to remote maven!!!")
-                repositories {
-                    mavenDeployer {
-                        beforeDeployment {
-                            if (publishExt.version == "" || publishExt.version == null) {
-                                throw new IllegalArgumentException("the version property in mavenPublish must not be null")
+            if (publishExt.version == "" || publishExt.version == null) {
+                return
+            }
+
+            if (publishExt.localPublish) {
+                println("publish to local maven")
+                targetProject.publishing {
+                    repositories {
+                        mavenLocal()
+                    }
+
+                    publications {
+                        maven(MavenPublication) {
+                            artifact "${targetProject.buildDir}/outputs/aar/${targetProject.name}-${publishExt.flavor}-release.aar"
+                            groupId publishExt.groupId
+                            artifactId getArtifactName(targetProject, publishExt.artifactId)
+                            version publishExt.version
+                        }
+                    }
+                }
+            } else {
+                println("publish to remote maven")
+                String usernameTemp = publishExt.userName
+                String pwd = publishExt.password
+                String versionTemp = publishExt.version
+                String artifactIdTemp = publishExt.artifactId
+                String groupIdTemp = publishExt.groupId
+                String urlPath
+                if (versionTemp.contains("SNAPSHOT")) {
+                    urlPath = publishExt.snapshotRepo
+                } else {
+                    urlPath = publishExt.releaseRepo
+                }
+
+                targetProject.publishing {
+                    repositories {
+                        maven {
+                            credentials {
+                                username usernameTemp // 仓库发布用户名
+                                password pwd // 仓库发布用户密码
                             }
-                            { MavenDeployment deployment -> signing.signPom(deployment) }
+                            url urlPath // 仓库地址
                         }
-                        pom.groupId = publishExt.groupId
-                        pom.artifactId = EnjoyMavenPublishPlugin.getArtifactName(targetProject, publishExt.artifactId)
-                        pom.version = publishExt.version
+                    }
 
-                        repository(url: publishExt.releaseRepo) {
-                            authentication(userName: publishExt.userName, password: publishExt.password)
-                        }
-                        snapshotRepository(url: publishExt.snapshotRepo) {
-                            authentication(userName: publishExt.userName, password: publishExt.password)
-                        }
-
-                        pom.project {
-                            name EnjoyMavenPublishPlugin.getArtifactName(targetProject, publishExt.artifactId)
-                            packaging EnjoyMavenPublishPlugin.getPackageType(targetProject)
+                    publications {
+                        maven(MavenPublication) {
+                            groupId groupIdTemp
+                            artifactId artifactIdTemp
+                            version versionTemp
+                            artifact "${targetProject.buildDir}/outputs/aar/${targetProject.name}-${publishExt.flavor}-release.aar"
                         }
                     }
                 }
-            }
-
-            targetProject.artifacts {
-                println("我执行了")
-//                archives androidSourcesJar
-            }
-
-            targetProject.publishing {
-                print("publish to local maven!!!")
-                repositories {
-                    mavenLocal()
-                }
-
-                publications {
-                    maven(MavenPublication) {
-                        artifact "${targetProject.buildDir}/outputs/aar/${targetProject.name}-release.aar"
-                        groupId  publishExt.groupId
-                        artifactId getArtifactName(targetProject, publishExt.artifactId)
-                        version publishExt.version
-                    }
-                }
-            }
-
-            targetProject.signing {
-                required {
-                    isReleaseBuild(publishExt.version) && targetProject.gradle.taskGraph.hasTask("uploadArchives")
-                }
-                sign targetProject.configurations.archives
             }
         }
     }
--- enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/MavenPublishExt.groovy
+++ enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/MavenPublishExt.groovy
@@ -4,11 +4,13 @@ package com.youzan.mobile.enjoydependence
  * config properties that uploadArchives pom file needed
  */
 class MavenPublishExt {
+    String flavor//phone or pad
     String version
     String artifactId
     String groupId
     String userName
     String password
+    boolean localPublish//本地发布
     String releaseRepo
     String snapshotRepo
 }
