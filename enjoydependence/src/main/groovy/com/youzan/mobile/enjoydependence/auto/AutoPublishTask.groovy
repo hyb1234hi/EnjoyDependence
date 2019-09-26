f429f0509ce9f8021f55d598ced346e75274f69e
liuyang_ly <liuyang_ly@youzan.com>
2019-09-26 14:47:59 +0800

    自动发布并回写版本号

--- enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/auto/AutoPublishTask.groovy
+++ enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/auto/AutoPublishTask.groovy
@@ -7,8 +7,50 @@ import org.gradle.api.tasks.TaskAction
 
 class AutoPublishTask extends DefaultTask {
 
+    String defaultVersion = ""
+
     @TaskAction
     void autoPublish() {
+        defaultVersion = project.hasProperty("version") ? project.version : defaultVersion
+        if (defaultVersion != "") {
+            //写入到project的version.property
+            def list = []
+            File file = new File("version.properties")
+            if (file.exists()) {
+                file.withReader('UTF-8') { reader ->
+                    reader.eachLine {
+                        if (it.contains("versionName")) {
+                            it = "versionName=${defaultVersion}"
+                            list.add(it + "\n")
+                        }
+                    }
+                }
+                file.withWriter('UTF-8') { writer ->
+                    list.each {
+                        writer.write(it)
+                    }
+                }
+            }
+
+            def versions = []
+            File rootVersionFile = new File(project.rootProject.projectDir.absolutePath + "/" + "version.properties")
+            if (rootVersionFile.exists()) {
+                rootVersionFile.withReader('UTF-8') { reader ->
+                    reader.eachLine {
+                        if (it.contains(project.name)) {
+                            it = "${project.name}=${defaultVersion}"
+                            versions.add(it + "\n")
+                        }
+                    }
+                }
+                rootVersionFile.withWriter('UTF-8') { writer ->
+                    versions.each {
+                        writer.write(it)
+                    }
+                }
+            }
+        }
+        println("version: ${defaultVersion}")
         println("-----------------auto publish finish-------------------")
     }
 
