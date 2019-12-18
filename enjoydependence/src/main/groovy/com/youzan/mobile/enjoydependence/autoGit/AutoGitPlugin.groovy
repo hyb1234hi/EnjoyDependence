e2cda59590d56f6d0d4dddc096fc9a8f571f314e
liuyang_ly <liuyang_ly@youzan.com>
2019-12-18 16:01:16 +0800

    autoMr增加企业微信通知功能

--- enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/autoGit/AutoGitPlugin.groovy
+++ enjoydependence/src/main/groovy/com/youzan/mobile/enjoydependence/autoGit/AutoGitPlugin.groovy
@@ -3,10 +3,13 @@ package com.youzan.mobile.enjoydependence.autoGit
 import groovy.json.JsonSlurper
 import groovyx.net.http.FromServer
 import groovyx.net.http.HttpBuilder
+import groovyx.net.http.OkHttpBuilder
+import groovyx.net.http.OkHttpEncoders
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 import static groovyx.net.http.ContentTypes.JSON
+import static groovyx.net.http.MultipartContent.multipart
 
 /**
  * git自动化plugin
@@ -21,6 +24,15 @@ class AutoGitPlugin implements Plugin<Project> {
      * 默认目标分支
      */
     String defTargetBranch
+
+    //零售git工程
+    def projectUrl = "http://gitlab.qima-inc.com/normandy-android/NewRetail/merge_requests"
+    //零售HD MBD构建任务
+    def newRetailHDMBDUrl = "https://mbd.qima-inc.com/#/integration/3901"
+    //零售 MBD构建任务
+    def newRetailMBDUrl = "https://mbd.qima-inc.com/#/integration/3900"
+    //默认userEmail
+    def userEmail = "liuyang_ly@youzan.com"
 
     @Override
     void apply(Project project) {
@@ -38,6 +50,9 @@ class AutoGitPlugin implements Plugin<Project> {
             }
             if (project.hasProperty("target_branch") && project.target_branch != "unspecified") {
                 defTargetBranch = project.target_branch
+            }
+            if (project.hasProperty("userEmail") && project.userEmail != "unspecified") {
+                userEmail = project.userEmail
             }
 
             project.getTasks().create("autoMr", AutoCreateMrTask.class).doFirst {
@@ -74,20 +89,24 @@ class AutoGitPlugin implements Plugin<Project> {
                                     println("-----------------auto accept mr success----------------")
                                     triggerBuild(autoGitExt.version)
                                     println("-----------------auto accept mr over----------------")
+                                    sendSuccessMessage("${userEmail}", "恭喜你，第一阶段成功完成，即将开始第二阶段", "成功触发构建提测包，请继续跟进","${newRetailHDMBDUrl}")
                                 }
                                 response.exception { t ->
                                     println("-----------------accept mr error: ${t.getMessage()}----------------")
+                                    sendFailureMessage("${userEmail}", "Accept MR Failure", "merge request合并失败", "${body.web_url}")
                                     throw new RuntimeException(t)
                                 }
                             }
                         } else {
                             println("-----------------body is null or iid is null----------------")
+                            sendFailureMessage("${userEmail}", "Create MR Failure", "自动创建merge request失败", "${projectUrl}")
                             throw new RuntimeException("body is null or iid is null")
                         }
                     }
 
                     response.exception { t ->
                         println("-----------------creat mr error: ${t.getMessage()}----------------")
+                        sendFailureMessage("${userEmail}", "Create MR Failure", "自动创建merge request失败", "${projectUrl}")
                         throw new RuntimeException(t)
                     }
                 }
@@ -110,6 +129,53 @@ class AutoGitPlugin implements Plugin<Project> {
         } catch (ignored) {
             println("-----------------trigger build error ----------------")
             return ""
+        }
+    }
+
+    def sendSuccessMessage(String userEmail, String title, String desc, String url) {
+        sendMessage("${userEmail}", "{\"status\":\"success\",\"title\":\"${title}\",\"desc\":\"${desc}\",\"url\":\"${url}\"}")
+    }
+
+    def sendFailureMessage(String userEmail, String title, String desc, String url) {
+        sendMessage("${userEmail}", "{\"status\":\"failure\",\"title\":\"${title}\",\"desc\":\"${desc}\",\"url\":\"${url}\"}")
+    }
+
+    /**
+     * 发送消息
+     * @param title 标题
+     * @param desc 描述
+     */
+    private void sendMessage(String userEmail, String desc) {
+        if (userEmail == null || userEmail.isEmpty()) {
+            return
+        }
+        if (desc == null || desc.isEmpty()) {
+            return
+        }
+        AutoMessage autoMessage = new JsonSlurper().parseText(desc)
+        String msg
+        if (autoMessage.status == "success") {
+            msg = "[SUCCESS] 当前进度正常，请继续关注： <a href=${autoMessage.url}>${autoMessage.title}</a> , ${autoMessage.desc}"
+        } else {
+            msg = "[FAILURE] 构造失败，请跟进： <a href=${autoMessage.url}>${autoMessage.title}</a> , ${autoMessage.desc}"
+        }
+
+        OkHttpBuilder.configure {
+            request.uri = 'http://retail.prod.qima-inc.com/wechat/notification/post'
+        }.post {
+            request.contentType = 'multipart/form-data'
+            request.body = multipart {
+                field 'touser', "${userEmail}"
+                part 'content', "${msg}"
+            }
+            request.encoder 'multipart/form-data', OkHttpEncoders.&multipart
+            response.success { FromServer fs, Object body ->
+                println("-----------------send message success: ${body.desc}----------------")
+            }
+            response.exception { t ->
+                println("-----------------send message error: ${t.getMessage()}----------------")
+                throw new RuntimeException(t)
+            }
         }
     }
 }
